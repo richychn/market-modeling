3e186a35108101b274e7c7162e3914e438fbc4ce
jasony070 <jasony070@gmail.com>
2019-04-11 14:33:38 -0700

    update

--- static/js/graph.js
+++ static/js/graph.js
@@ -14,6 +14,7 @@ var data = {
   ]
 };
 
+
 // Create a new line chart object where as first parameter we pass in a selector
 // that is resolving to our chart container element. The Second parameter
 // is the actual data object.
