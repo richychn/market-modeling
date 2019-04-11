641027f02f6b77b581e9e50c2176f30034a81954
jasony070 <jasony070@gmail.com>
2019-04-10 22:44:48 -0700

    legend colors

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,8 +3,14 @@ var data = {
   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
   // Our series array that contains series objects or in this case series data arrays
   series: [
-    [5, 2, 4, 2, 0],
-    [2, 1, 4, 7, 8]
+        [12, 9, 7, 8, 5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6],
+        [7, 2, 8, 15, 1],
+        [18, 3, 7, 2, 8],
+        [1, 9, 11, 5, 3],
+        [2, 16, 4, 9, 3],
+        [5, 14, 3, 9, 5]
   ]
 };
 
@@ -12,3 +18,5 @@ var data = {
 // that is resolving to our chart container element. The Second parameter
 // is the actual data object.
 new Chartist.Line('.ct-chart', data);
+
+
