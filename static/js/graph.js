2a5e3dd0d2ae04b89b5e8d6222ea38eee57c0cdc
jasony070 <jasony070@gmail.com>
2019-04-10 17:47:28 -0700

    update

--- static/js/graph.js
+++ static/js/graph.js
@@ -2,11 +2,9 @@ var data = {
   // A labels array that can contain any sort of values
   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
   // Our series array that contains series objects or in this case series data arrays
-  series-a: [
+  series: [
     [5, 2, 4, 2, 0],
-  ]
-  series-b: [
-    [1, 2, 3, 2, 0],
+    [2, 1, 4, 7, 8]
   ]
 };
 
