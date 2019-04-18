e3419e02ada21aacb6ac87543b9bfaf9b293ceec
richychn <richychn@hotmail.com>
2019-04-17 21:24:24 -0700

    tried 10 predict 1 lstm

--- predict.py
+++ predict.py
@@ -33,7 +33,7 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     return agg
 
 def setup():
-    data = pd.read_csv(filepath_or_buffer="./static/data.csv", index_col="date")
+    data = pd.read_csv(filepath_or_buffer="./static/data2.csv", index_col="date")
     data = data.apply(pd.to_numeric, errors = "coerce")
     data['spindx'].replace(0, np.nan, inplace=True)
     data['spindx'].fillna(method='ffill', inplace=True)
@@ -43,17 +43,31 @@ def setup():
     values = values.astype('float32')
 
     historic_data = np.array([])
-    for day in values[-3:]:
+    for day in values[-10:]:
         historic_data = np.concatenate((historic_data, day), axis=None)
 
-    return values, historic_data.reshape(1,1,historic_data.shape[0])
+    historic_data = np.append (historic_data, historic_data[:8])
+    look_back = 10
+    time_steps = 1
+    reframed = series_to_supervised(values, look_back, time_steps)
+
+    return values, reframed, historic_data
+    # return values, historic_data.reshape(1,1,historic_data.shape[0])
 
 def predict(num=1):
     num = int(num)
-    values, pred_para = setup()
+    # values, pred_para = setup()
+    values, reframed, historic_data = setup()
+
+    reframed = reframed.append(dict(zip(reframed.columns, historic_data)), ignore_index=True)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_pred = scaler.fit_transform(reframed)
+
+    pred_para = scaled_pred[-1][:-8]
+    pred_para = pred_para.reshape(1,10,8)
 
     backend.clear_session()
-    multi_model = load_model("./static/lstm.hdf5")
+    multi_model = load_model("./static/lstm2.hdf5")
 
     graph = []
     for row in values[-30:]:
@@ -61,19 +75,28 @@ def predict(num=1):
 
     while num != 0:
       yhat = multi_model.predict(pred_para)
-      graph.append(yhat[0][0])
-      pred_para = np.concatenate((pred_para.flatten()[8:], yhat[0][0]))
-      pred_para = pred_para.reshape(1,1, pred_para.shape[0])
+
+      pred_para = pred_para.reshape((1,80))
+      pred = concatenate((pred_para[:, :], yhat), axis=1)
+      inv_pred = scaler.inverse_transform(pred)
+      graph.append(inv_pred[:,-8:].flatten())
+      pred_para = np.concatenate((pred_para.flatten()[8:], yhat[0]))
+      pred_para = pred_para.reshape(1,10,8)
+
+      # graph.append(yhat[0][0])
+      # pred_para = np.concatenate((pred_para.flatten()[8:], yhat[0][0]))
+      # pred_para = pred_para.reshape(1,1, pred_para.shape[0])
+
       num -= 1
 
     chartist = [[],[],[],[],[],[],[],[]]
     for row in graph:
       chartist[0].append(row[0])
-      chartist[1].append(row[1])
-      chartist[2].append(row[2])
-      chartist[3].append(row[3])
-      chartist[4].append(row[4])
-      chartist[5].append(row[5])
-      chartist[6].append(row[6])
-      chartist[7].append(row[7])
+      chartist[4].append(row[1])
+      chartist[5].append(row[2])
+      chartist[7].append(row[3])
+      chartist[6].append(row[4])
+      chartist[3].append(row[5])
+      chartist[1].append(row[6])
+      chartist[2].append(row[7])
     return chartist
