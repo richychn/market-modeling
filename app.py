06e8ac6050d988ce48a8a339a43ef0ec40db2ac2
richychn <richychn@hotmail.com>
2019-04-24 19:12:30 -0700

    pylint errors fixed

--- app.py
+++ app.py
@@ -1,47 +1,48 @@
+# pylint: disable=C0111
+
 import flask
 import predict
 
-app = flask.Flask(__name__, template_folder="templates")
+APP = flask.Flask(__name__, template_folder="templates")
 
-@app.route("/")
+@APP.route("/")
 def home():
-  return flask.render_template('main.html')
+    return flask.render_template('main.html')
 
-@app.route("/howto")
+@APP.route("/howto")
 def howto():
-  return flask.render_template('howto.html')
+    return flask.render_template('howto.html')
 
-@app.route("/aboutus")
+@APP.route("/aboutus")
 def aboutus():
-  return flask.render_template('aboutus.html')
+    return flask.render_template('aboutus.html')
 
-@app.route("/api/v1/predict_levels", methods=['POST'])
+@APP.route("/api/v1/predict_levels", methods=['POST'])
 def prediction_levels():
-  request = flask.request.get_json(force=True)
-  if isinstance(request, dict):
-    num = request.get("num", 1)
-    data = predict.predict_levels(int(num))
-    response = {}
-    for i in range(len(data)):
-      response[str(i)] = str(data[i])
-  else:
-    response = {
-      "error": "Invalid JSON"
-    }
-  return flask.jsonify(response)
+    request = flask.request.get_json(force=True)
+    if isinstance(request, dict):
+        num = request.get("num", 1)
+        data = predict.predict_levels(int(num))
+        response = {}
+        for i,_ in enumerate(data):
+            response[str(i)] = str(data[i])
+    else:
+        response = {
+            "error": "Invalid JSON"
+        }
+    return flask.jsonify(response)
 
-@app.route("/api/v1/predict_growth", methods=['POST'])
+@APP.route("/api/v1/predict_growth", methods=['POST'])
 def prediction_growth():
-  request = flask.request.get_json(force=True)
-  if isinstance(request, dict):
-    num = request.get("num", 1)
-    data = predict.predict_growth(int(num))
-    response = {}
-    for i in range(len(data)):
-      response[str(i)] = str(data[i])
-  else:
-    response = {
-      "error": "Invalid JSON"
-    }
-  return flask.jsonify(response)
-
+    request = flask.request.get_json(force=True)
+    if isinstance(request, dict):
+        num = request.get("num", 1)
+        data = predict.predict_growth(int(num))
+        response = {}
+        for i,_ in enumerate(data):
+            response[str(i)] = str(data[i])
+    else:
+        response = {
+            "error": "Invalid JSON"
+        }
+    return flask.jsonify(response)
