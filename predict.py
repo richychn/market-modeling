cc1dc604c72a7185ed2cd668925d9928d4f6ed18
richychn <richychn@hotmail.com>
2019-04-27 00:33:37 -0700

    new models, with two esn working for growth and levels

--- predict.py
+++ predict.py
@@ -5,11 +5,40 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import load_model
-from keras import backend
+from keras import backend as K
 from numpy import concatenate
 from numpy import append
 from numpy import array
 from numpy import nan
+
+def sign_ae(x, y):
+    sign_x = K.sign(x)
+    sign_y = K.sign(y)
+    delta = x - y
+    return sign_x * sign_y * K.abs(delta)
+
+
+def linex_loss(delta, a=-1, b=1):
+    if a!= 0 and b > 0:
+        loss = b * (K.exp(a * delta) - a * delta - 1)
+        return loss
+    else:
+        raise ValueError
+
+
+def linex_loss_val(y_true, y_pred):
+    delta = sign_ae(y_true, y_pred)
+    res = linex_loss(delta)
+    return K.mean(res)
+
+
+def linex_loss_ret(y_true, y_pred):
+    diff_true = y_true[1:] - y_true[:-1]
+    diff_pred = y_pred[1:] - y_pred[:-1]
+
+    delta = sign_ae(diff_true, diff_pred)
+    res = linex_loss(delta)
+    return K.mean(res)
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     """
@@ -41,25 +70,19 @@ def setup_levels():
       Output: Historical values, batched data, and unbatched data
     """
     data = pd.read_csv(filepath_or_buffer="./static/data2.csv", index_col="date")
-    data = data.apply(pd.to_numeric, errors="coerce")
+    data = data.apply(pd.to_numeric, errors = "coerce")
     data['spindx'].replace(0, nan, inplace=True)
     data['spindx'].fillna(method='ffill', inplace=True)
-
-    values = data[['spindx'] + ['TCMNOM_Y2'] + ['TCMNOM_Y10'] + ['DCOILBRENTEU']
-                  + ['GOLDPMGBD228NLBM'] + ['exalus'] + ['exjpus'] + ['exukus']].values
+    values = data[['spindx'] + ['TCMNOM_Y2'] + ['TCMNOM_Y10'] + ['DCOILBRENTEU'] + ['GOLDPMGBD228NLBM'] + ['exalus'] + ['exjpus'] + ['exukus']].values
     values = values.astype('float32')
-
     historic_data = array([])
-    for day in values[-10:]:
+    for day in values[-20:]:
         historic_data = concatenate((historic_data, day), axis=None)
-
-    historic_data = append (historic_data, historic_data[:8])
+    historic_data = append (historic_data, historic_data)
     look_back = 10
-    time_steps = 1
+    time_steps = 10
     reframed = series_to_supervised(values, look_back, time_steps)
-
-    return values, reframed, historic_data
-    # return values, historic_data.reshape(1,1,historic_data.shape[0])
+    return reframed, values, time_steps
 
 def predict_levels(num=1):
     """
@@ -67,39 +90,28 @@ def predict_levels(num=1):
       Input: num represents the number of days to predict
       Output: An array of last 30 days of historical data plus predictions
     """
-    num = int(num)
-    # values, pred_para = setup()
-    values, reframed, historic_data = setup_levels()
-
-    reframed = reframed.append(dict(zip(reframed.columns, historic_data)), ignore_index=True)
+    reframed, values, time_steps = setup_levels()
     scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_pred = scaler.fit_transform(reframed)
-
-    pred_para = scaled_pred[-1][:-8]
+    scaled = scaler.fit_transform(reframed)
+    pred_para = scaled[-1][:-8 * time_steps]
     pred_para = pred_para.reshape(1,10,8)
 
-    backend.clear_session()
-    multi_model = load_model("./static/lstm2.hdf5")
+    K.clear_session()
+    multi_model = load_model("./static/8-8-ESN-L.hdf5", custom_objects={'linex_loss_ret': linex_loss_ret})
+    yhat = multi_model.predict(pred_para)
+    pred_para = pred_para.reshape((1,80))
+    yhat = yhat.reshape((1,80))
+    pred = concatenate((pred_para[:, :], yhat), axis=1)
+    inv_pred = scaler.inverse_transform(pred)
+    inv_pred = inv_pred[:,-8 * time_steps:].flatten()
+    inv_pred = inv_pred.reshape(10,8)
 
     graph = []
     for row in values[-30:]:
         graph.append(row)
 
-    while num != 0:
-        yhat = multi_model.predict(pred_para)
-
-        pred_para = pred_para.reshape((1,80))
-        pred = concatenate((pred_para[:, :], yhat), axis=1)
-        inv_pred = scaler.inverse_transform(pred)
-        graph.append(inv_pred[:,-8:].flatten())
-        pred_para = concatenate((pred_para.flatten()[8:], yhat[0]))
-        pred_para = pred_para.reshape(1,10,8)
-
-        # graph.append(yhat[0][0])
-        # pred_para = concatenate((pred_para.flatten()[8:], yhat[0][0]))
-        # pred_para = pred_para.reshape(1,1, pred_para.shape[0])
-
-        num -= 1
+    for row in inv_pred:
+      graph.append(row)
 
     chartist = [[],[],[],[],[],[],[],[]]
     for row in graph:
@@ -128,10 +140,11 @@ def setup_growth():
     values = values.astype('float32')
 
     historic_data = array([])
-    for day in values[-3:]:
+    timestep = 10
+    for day in values[-1 * timestep:]:
         historic_data = concatenate((historic_data, day), axis=None)
 
-    return values, historic_data.reshape(1,1,historic_data.shape[0])
+    return values, historic_data.reshape(1,timestep,8)
 
 def predict_growth(num=1):
     """
@@ -142,19 +155,18 @@ def predict_growth(num=1):
     num = int(num)
     values, pred_para = setup_growth()
 
-    backend.clear_session()
-    multi_model = load_model("./static/lstm.hdf5")
+    K.clear_session()
+    multi_model = load_model("./static/ESN_growth.hdf5", custom_objects={'linex_loss_val': linex_loss_val})
 
     graph = []
     for row in values[-30:]:
         graph.append(row)
 
-    while num != 0:
-        yhat = multi_model.predict(pred_para)
-        graph.append(yhat[0][0])
-        pred_para = concatenate((pred_para.flatten()[8:], yhat[0][0]))
-        pred_para = pred_para.reshape(1,1, pred_para.shape[0])
-        num -= 1
+    yhat = multi_model.predict(pred_para)
+    yhat_pred = yhat.reshape(10,8)
+
+    for row in yhat_pred:
+      graph.append(row)
 
     chartist = [[],[],[],[],[],[],[],[]]
     for row in graph:
